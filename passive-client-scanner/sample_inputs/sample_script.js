ba752d94ed989435ebb2ca9ec94e9799a77eaf8c
KTraylor_GITHUB <128261955+keigan-traylor@users.noreply.github.com>
2025-10-15 13:10:44 -0400

    Delete passive-client-scanner/sample_inputs/sample_script.js

--- passive-client-scanner/sample_inputs/sample_script.js
+++
@@ -1,4 +0,0 @@
-// sample_script.js
-var sql = "SELECT * FROM users WHERE id='" + userId + "'";
-element.innerHTML = userProvidedHtml; // unsafe pattern for demo only
-function dangerous(){ eval('console.log(1)'); }
